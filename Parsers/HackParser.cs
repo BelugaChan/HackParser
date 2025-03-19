5550e960b2514ba4fb47aed550d085bb3618564d
BelugaChan <tuchnyy@list.ru>
2025-03-19 17:26:23 +0300

    Extended parser logic

--- Parsers/HackParser.cs
+++ Parsers/HackParser.cs
@@ -1,12 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using HtmlAgilityPack;
 using parser.DTO;
 using parser.Interfaces.Factory;
+using parser.Interfaces.Handlers;
 using parser.Interfaces.Models;
 using parser.Interfaces.Parsers;
+using parser.Models;
 using PuppeteerSharp;
 
 namespace parser.Parsers
@@ -16,12 +13,14 @@ namespace parser.Parsers
     {
         private static List<THack> hacks = new();
         private readonly IHackFactory<THack> factory;
-        public HackParser(IHackFactory<THack> factory)
+        private readonly IInnerTextHandler textHandler;
+        public HackParser(IHackFactory<THack> factory, IInnerTextHandler textHandler)
         {
             this.factory = factory;
+            this.textHandler = textHandler;
         }
 
-        public async Task<List<THack>> ScrapHacks(string query, string siteName = "")
+        public async Task<List<THack>> ScrapHacks(string query, string siteName = "https://codenrock.com/")
         {
             ArgumentException.ThrowIfNullOrEmpty(siteName);
 
@@ -47,14 +46,39 @@ namespace parser.Parsers
                     var priseElement = await GetElement("xpath//.//div/a/article/ul/li[6]", hackData);
                     var teamLimitElement = await GetElement("xpath//.//div/a/article/ul/li[5]/div/div", hackData);
                     var dateElement = await GetElement("xpath//.//div/a/article/ul/li[2]/div/span[2]", hackData);
-                    var formatElement = await GetElement("xpath//.//div/a/article/ul/li[2]/div/span[2]", hackData);
+                    var formatElement = await GetElement("xpath//.//div/a/article/ul/li[4]/div/span[2]", hackData);
+
                     var info = await GetInnerText(infoElement);
                     var description = await GetInnerText(descriptionElement);
                     var prise = await GetInnerText(priseElement);
                     var teamLimit = await GetInnerText(teamLimitElement);
                     var date = await GetInnerText(dateElement);
                     var format = await GetInnerText(formatElement);
-                    var hack = factory.Create(new HackCreationParameters() { Id = Guid.NewGuid(), Description = description, });
+
+                    var teamLimits = textHandler.ToLimitFromInnerText(teamLimit);
+                    string[] dates = date.Split('-');
+                    List<DateTime> dateTimeDates = new();
+                    foreach (var item in dates)
+                    {
+                        dateTimeDates.Add(textHandler.ToDateTimeFromInnerText(item));
+                    }
+                    var hack = factory.Create(new HackCreationParameters()
+                    {
+                        Id = Guid.NewGuid(),
+                        Description = description,
+                        Info = info,
+                        Prise = textHandler.ToPriseFromInnerText(prise),
+                        MinLimitPeopleForTeams = teamLimits[0],
+                        MaxLimitPeopleForTeams = teamLimits[1],
+                        DateStart = dateTimeDates[0],
+                        DateEnd = dateTimeDates[1],
+                        Format = textHandler.ToHackTypeFromInnerText(format),
+                        Owner = new Organization()
+                        {
+                            Id = Guid.NewGuid()
+                        }
+                    });
+
                     hacks.Add(hack);
                 }
             }
@@ -69,7 +93,7 @@ namespace parser.Parsers
         {
             if (element is null)
                 return string.Empty;
-            return (await element.GetPropertyAsync("InnerText")).RemoteObject.Value.ToString();
+            return (await element.GetPropertyAsync("innerText")).RemoteObject.Value.ToString();
         }
     }
 }
